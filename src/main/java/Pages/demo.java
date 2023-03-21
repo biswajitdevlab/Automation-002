2b18d4b27e58408240a2abef390ac13ecd554ba2
soyeb <soyeb@DESKTOP-73JM3I5>
2023-03-21 19:07:38 +0530

    commit 2

---
+++ src/main/java/Pages/demo.java
@@ -0,0 +1,15 @@
+package Pages;
+
+public class demo {
+
+	public static void main(String[] args) {
+   String demo=null;
+if(demo=="yes") {
+	System.out.println("Accept");
+}
+else {
+	System.out.println("Reject");
+}
+	}
+
+}
